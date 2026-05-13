466486f2d10801d9df4338e486ecb8170e33bfac
BryanEphec <psr11274@students.ephec.be>
2026-05-14 01:16:24 +0200

    controller Site !

--- Controllers/SitesController.cs
+++ Controllers/SitesController.cs
@@ -24,6 +24,53 @@ namespace Padel.SGBD.Api.Controllers
             return await _context.Site.ToListAsync();
         }
 
+        [HttpPost]
+        public async Task<ActionResult<Site>> PostSite(Site site)
+        {
+            _context.Site.Add(site);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSite), new { id = site.IdSite }, site);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Site>> PutSite(int id, Site site)
+        {
+            if (id != site.IdSite)    return BadRequest();
+            
+
+            _context.Entry(site).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Site.Any(e => e.IdSite == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Site>> DeleteSite(int id)
+        {
+            var site = await _context.Site.FindAsync(id);
+            if (site == null)
+            {
+                return NotFound();
+            }
+
+            _context.Site.Remove(site);
+            await _context.SaveChangesAsync();
+
+            return site;
+        }
+
         // GET: api/SitesControllers/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Site>> GetSite(int id)
--- Model/Site.cs
+++ Model/Site.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Padel.SGBD.Api.Model
 {
     public class Site
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdSite { get; set; }
         public string Nom { get; set; } = String.Empty;
         public string Ville { get; set; } = String.Empty;
